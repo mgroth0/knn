657176bcca77a6b35a0f76f7e08d8a7ecf0c9389
mgroth0 <mgroth49@gmail.com>
2022-11-28 16:23:26 -0500

    autocommit

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -22,7 +22,8 @@ buildscript {
 	maven(
 	  url = "https://s01.oss.sonatype.org/content/repositories/releases/"
 	)
-  }/*this is necessary for libs.xmlutil.core and libs.xmlutil.serialization*/
+  }
+  /*this is necessary for libs.xmlutil.core and libs.xmlutil.serialization*/
   val androidAttribute = Attribute.of("net.devrieze.android", Boolean::class.javaObjectType)
   configurations.all {
 	attributes {
@@ -50,30 +51,18 @@ buildscript {
 	  "kbuild",
 	  "buildinfo",
 	  "mbuild",
+
 	).forEach { gradleMod ->
 
 	  val kbuildDir = registeredDir.resolve("gbuild/dist/$gradleMod")
 	  val numBack = prop("NUM_BACK").toInt()
 	  if (osName == "Windows 11") {
 		classpath(files("Y:\\$gradleMod.jar")) /*PROBABLY WONT WORK AFTER KBUILD DEPS LIST FILE UPDATE*/
-	  } else if (prop(
-		  "PARTIAL_BOOTSTRAP"
-		).toBoolean()
-	  ) {        //	  classpath(files(registeredDir.resolve("kbuild.jar"))) /*PROBABLY WONT WORK AFTER KBUILD DEPS LIST FILE UPDATE*/
-		classpath(
-		  files(registeredDir.resolve("gbuild/jar/$gradleMod.jar"))
-		) /*PROBABLY WONT WORK AFTER KBUILD DEPS LIST FILE UPDATE*/
 	  } else {
-
-		/*deps.txt is no longer reliable copied to bin/dist/$gradleMod/lib because it is copied in another task*/
-
-		/*val kbuildLibsFolder = if (numBack == 0) registeredDir.resolve("bin/dist/$gradleMod/lib")
-		else {*/
 		val recentVersion = kbuildDir.list()!!.mapNotNull {
 		  it.toLongOrNull()
 		}.sorted().reversed()[numBack]
 		val kbuildLibsFolder = kbuildDir.resolve("$recentVersion")
-		/*}*/
 		classpath(fileTree(kbuildLibsFolder))
 		val deps = kbuildLibsFolder.resolve("deps.txt").readLines().filter { it.isNotBlank() }.map {
 		  val parts = it.split(":")
